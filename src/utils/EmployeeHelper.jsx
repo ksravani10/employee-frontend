67f597074e467e4c874682616424e43a723a77f0
Ksravani10 <kesavarapu2000@gmail.com>
2025-11-20 23:43:16 +0530

    url changes

--- src/utils/EmployeeHelper.jsx
+++ src/utils/EmployeeHelper.jsx
@@ -39,7 +39,7 @@ export const columns = [
 export const fetchDepartments = async () => {
   let departments;
   try {
-    const response = await axios.get("https://employee-server-kohl-one.vercel.app/department", {
+    const response = await axios.get("https://employee-server-kohl-one.vercel.app/api/department", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
